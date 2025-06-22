9c69393dbf83b21132342ac8e33821c3921a0fdc
Genga Chris <145026140+Gengachris@users.noreply.github.com>
2025-06-22 16:50:39 +0200

    Add solution - CodeHub

---
+++ 6-kyu/array-exchange/array-exchange.js
@@ -0,0 +1,10 @@
+function exchangeWith(a, b) {
+  // your code here
+  const result =[];
+  let reversedA = a.reverse();
+  let reversedB =b.reverse();
+  reversedA = result;
+  return reversedB = reversedA;
+  return reversedA = reversedB;
+}
+console.log(exchangeWith(['a','b','c'],['3','2','1']))
