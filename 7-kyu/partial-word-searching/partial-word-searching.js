2a569c93754e7a2095ee43da65413a64cd757725
Genga Chris <145026140+Gengachris@users.noreply.github.com>
2025-06-23 00:07:08 +0200

    Update solution - CodeHub

--- 7-kyu/partial-word-searching/partial-word-searching.js
+++ 7-kyu/partial-word-searching/partial-word-searching.js
@@ -1,4 +1,5 @@
-function wordSearch(query, seq){
- return seq.filter(elem => elem.toLowerCase().includes(query.toLowerCase()))
+function wordSearch(query, seq){ 
+const result = seq.filter(elem => elem.toLowerCase().includes(query.toLowerCase()))
+return result.length > 0 ? result : "Empty";
 }
 console.log(wordSearch('muh',['chris','christopher','christine','muhizi']));
