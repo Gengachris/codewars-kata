b147f3c7fc6d936de5bba5cfeeeb36398bff9b12
Genga Chris <145026140+Gengachris@users.noreply.github.com>
2026-03-31 23:34:13 +0200

    Add solution - CodeHub

---
+++ 8-kyu/training-js-number-8-conditional-statement-switch/training-js-number-8-conditional-statement-switch.js
@@ -0,0 +1,24 @@
+        break; 
+      case 7:
+        days = 31;
+        break;  
+      case 8:
+        days = 31;
+        break;
+      case 9:
+        days = 30;
+        break;
+      case 10:
+        days = 31;
+        break; 
+      case 11:
+        days = 30;
+        break; 
+      case 12:
+        days = 31;
+        break;    
+      default:
+      console.log(" month will always be greater than 0 and less than or equal to 12. ");
+  }
+  return days;
+}
